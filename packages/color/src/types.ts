1b27aa06091b240794d5ee75850097209371243f
r-sp <36327242+r-sp@users.noreply.github.com>
2025-03-12 23:39:59 +0700

    add color format and css decimal rules

--- packages/color/src/types.ts
+++ packages/color/src/types.ts
@@ -16,8 +16,31 @@ type ColorSpace<T extends ColorKind> = [number, number, number] & {
 
 type ColorMode = Exclude<ColorKind, "hsv" | "lrgb" | "xyz50">;
 
+type ColorFormat<T extends ColorMode> = [T, number, number, number] & {
+  readonly mode: T;
+};
+
 type CssColor<T extends ColorKind> = string & {
   readonly mode: T;
 };
 
-export type { ColorKind, ColorSpace, ColorMode, CssColor };
+interface CssDecimal {
+  rgb: "default";
+  hsl: "degree" | "percent" | "percent-degree";
+  hwb: "degree" | "percent" | "percent-degree";
+  lab: "percent";
+  lch: "degree" | "percent" | "percent-degree";
+  oklab: "percent";
+  oklch: "degree" | "percent" | "percent-degree";
+}
+
+type CssConfig<T extends ColorMode> = CssDecimal[T];
+
+export type {
+  ColorKind,
+  ColorSpace,
+  ColorMode,
+  ColorFormat,
+  CssColor,
+  CssConfig,
+};
