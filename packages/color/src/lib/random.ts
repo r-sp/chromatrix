703039fef235f5ae6738675cc21e39c93b1097c2
r-sp <36327242+r-sp@users.noreply.github.com>
2025-03-16 23:35:00 +0700

    feat: shuffle color space

--- packages/color/src/lib/random.ts
+++ packages/color/src/lib/random.ts
@@ -1,5 +1,6 @@
 import type { ColorMode, ColorSpace } from "../types";
-import { colorRange } from "./gamut";
+import { convertColor } from "./convert";
+import { colorKind, colorRange } from "./gamut";
 
 const createPRNG = (value: number): { token: () => number } => {
   let current = value;
@@ -34,9 +35,7 @@ const randomColor = <T extends ColorMode>(
 };
 
 const randomMode = (prng: { token: () => number }): ColorMode => {
-  const base: ColorMode[] = ["rgb", "hsl", "hwb", "lab", "lch", "oklab", "oklch"];
-
-  const index = Array.from({ length: base.length }, (_, i) => i);
+  const index = Array.from({ length: colorKind.length }, (_, i) => i);
   const shuffle: number[] = [];
 
   let i = index.length;
@@ -48,10 +47,117 @@ const randomMode = (prng: { token: () => number }): ColorMode => {
 
   let mode: ColorMode = "rgb";
   for (const color of shuffle) {
-    mode = base[color] || mode;
+    mode = colorKind[color] || mode;
   }
 
   return mode;
 };
 
-export { createPRNG, createToken, randomColor, randomMode };
+const shuffleColor = (prng: { token: () => number }): [
+  {
+    [T in ColorMode]: ColorSpace<T>;
+  },
+  ColorMode,
+] => {
+  const mode = randomMode(prng);
+  const current = randomColor(mode, prng);
+  let color: { [I in ColorMode]: ColorSpace<I> };
+
+  switch (mode) {
+    case "rgb": {
+      const rgb = current as ColorSpace<"rgb">;
+      color = {
+        rgb: rgb,
+        hsl: convertColor(rgb, "hsl"),
+        hwb: convertColor(rgb, "hwb"),
+        lab: convertColor(rgb, "lab"),
+        lch: convertColor(rgb, "lch"),
+        oklab: convertColor(rgb, "oklab"),
+        oklch: convertColor(rgb, "oklch"),
+      };
+      break;
+    }
+    case "hsl": {
+      const hsl = current as ColorSpace<"hsl">;
+      color = {
+        rgb: convertColor(hsl, "rgb"),
+        hsl: hsl,
+        hwb: convertColor(hsl, "hwb"),
+        lab: convertColor(hsl, "lab"),
+        lch: convertColor(hsl, "lch"),
+        oklab: convertColor(hsl, "oklab"),
+        oklch: convertColor(hsl, "oklch"),
+      };
+      break;
+    }
+    case "hwb": {
+      const hwb = current as ColorSpace<"hwb">;
+      color = {
+        rgb: convertColor(hwb, "rgb"),
+        hsl: convertColor(hwb, "hsl"),
+        hwb: hwb,
+        lab: convertColor(hwb, "lab"),
+        lch: convertColor(hwb, "lch"),
+        oklab: convertColor(hwb, "oklab"),
+        oklch: convertColor(hwb, "oklch"),
+      };
+      break;
+    }
+    case "lab": {
+      const lab = current as ColorSpace<"lab">;
+      color = {
+        rgb: convertColor(lab, "rgb"),
+        hsl: convertColor(lab, "hsl"),
+        hwb: convertColor(lab, "hwb"),
+        lab: lab,
+        lch: convertColor(lab, "lch"),
+        oklab: convertColor(lab, "oklab"),
+        oklch: convertColor(lab, "oklch"),
+      };
+      break;
+    }
+    case "lch": {
+      const lch = current as ColorSpace<"lch">;
+      color = {
+        rgb: convertColor(lch, "rgb"),
+        hsl: convertColor(lch, "hsl"),
+        hwb: convertColor(lch, "hwb"),
+        lab: convertColor(lch, "lab"),
+        lch: lch,
+        oklab: convertColor(lch, "oklab"),
+        oklch: convertColor(lch, "oklch"),
+      };
+      break;
+    }
+    case "oklab": {
+      const oklab = current as ColorSpace<"oklab">;
+      color = {
+        rgb: convertColor(oklab, "rgb"),
+        hsl: convertColor(oklab, "hsl"),
+        hwb: convertColor(oklab, "hwb"),
+        lab: convertColor(oklab, "lab"),
+        lch: convertColor(oklab, "lch"),
+        oklab: oklab,
+        oklch: convertColor(oklab, "oklch"),
+      };
+      break;
+    }
+    case "oklch": {
+      const oklch = current as ColorSpace<"oklch">;
+      color = {
+        rgb: convertColor(oklch, "rgb"),
+        hsl: convertColor(oklch, "hsl"),
+        hwb: convertColor(oklch, "hwb"),
+        lab: convertColor(oklch, "lab"),
+        lch: convertColor(oklch, "lch"),
+        oklab: convertColor(oklch, "oklab"),
+        oklch: oklch,
+      };
+      break;
+    }
+  }
+
+  return [color, mode];
+};
+
+export { createPRNG, createToken, randomColor, randomMode, shuffleColor };
