390fd465c5b1a8320ddfa001d6e94561a0b3c124
r-sp <36327242+r-sp@users.noreply.github.com>
2025-03-16 23:36:01 +0700

    chore: update bundle color utils

--- packages/color/src/index.ts
+++ packages/color/src/index.ts
@@ -2,20 +2,22 @@ export { rgbToHex, hexToRgb } from "./mode/rgb";
 
 export { rgbToHsv, hsvToRgb } from "./mode/hsv";
 
-export { hsvToHsl, hslToHsv } from "./mode/hsl";
+export { hsvToHsl, hslToHsv, lrgbToHsl, hslToLrgb } from "./mode/hsl";
 
-export { hsvToHwb, hwbToHsv } from "./mode/hwb";
+export { hsvToHwb, hwbToHsv, lrgbToHwb, hwbToLrgb } from "./mode/hwb";
 
 export { rgbToLrgb, lrgbToRgb } from "./mode/lrgb";
 
 export { lrgbToXyz50, xyz50ToLrgb } from "./mode/xyz50";
 
-export { xyz50ToLab, labToXyz50 } from "./mode/lab";
+export { xyz50ToLab, labToXyz50, lrgbToLab, labToLrgb } from "./mode/lab";
 
-export { labToLch, lchToLab } from "./mode/lch";
+export { labToLch, lchToLab, lrgbToLch, lchToLrgb } from "./mode/lch";
 
 export { lrgbToOklab, oklabToLrgb } from "./mode/oklab";
 
-export { oklabToOklch, oklchToOklab } from "./mode/oklch";
+export { oklabToOklch, oklchToOklab, lrgbToOklch, oklchToLrgb } from "./mode/oklch";
 
-export { createPRNG, createToken, randomColor, randomMode } from "./lib/random";
+export { convertColor } from "./lib/convert";
+
+export { createPRNG, createToken, randomColor, randomMode, shuffleColor } from "./lib/random";
