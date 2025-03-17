66957c0fc614b38c5c6afc2e56def4a099136b92
r-sp <36327242+r-sp@users.noreply.github.com>
2025-03-17 23:01:37 +0700

    test: add shuffle color space with gamut

--- packages/coverage/src/index.ts
+++ packages/coverage/src/index.ts
@@ -1,9 +1,28 @@
-import { createPRNG, shuffleColor } from "@repo/color/fn";
+import { createPRNG, randomColor, randomMode } from "@repo/color/fn";
+import { nearest } from "@repo/color/utils";
+import { shuffleColor } from "./shuffle";
 
-const token = createPRNG(Date.now());
+const date = new Date();
+const seconds = date.getSeconds();
+const minutes = date.getMinutes();
+const recents = seconds + minutes;
+
+const token = createPRNG(recents);
+
+const mode = randomMode(token);
+const base = randomColor(mode, token);
 
 console.time("benchmark");
 
-const color = shuffleColor(token);
+const color = shuffleColor(base);
 
-console.timeLog("benchmark", color);
+console.timeLog("benchmark", {
+  mode,
+  rgb: nearest(color.rgb, 3),
+  hsl: nearest(color.hsl, 3),
+  hwb: nearest(color.hwb, 3),
+  lab: nearest(color.lab, 3),
+  lch: nearest(color.lch, 3),
+  oklab: nearest(color.oklab, 3),
+  oklch: nearest(color.oklch, 3),
+});
