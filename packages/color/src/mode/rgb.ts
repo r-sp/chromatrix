5e2d35944da46deec35f51916b9a8cfb176101cf
r-sp <36327242+r-sp@users.noreply.github.com>
2025-03-23 23:00:03 +0700

    style: color format rgb to hex

--- packages/color/src/mode/rgb.ts
+++ packages/color/src/mode/rgb.ts
@@ -1,21 +1,24 @@
-import type { ColorSpace } from "../types";
+import type { ColorFormat } from "../types";
 
-const rgbToHex = (input: ColorSpace<"rgb">) => {
-  return ((1 << 24) | (input[0] << 16) | (input[1] << 8) | input[2]).toString(16).slice(1);
+const rgbToHex = (input: ColorFormat<"rgb">, denote = false): string => {
+  const hex = ((1 << 24) | (input[1] << 16) | (input[2] << 8) | input[3]).toString(16).slice(1);
+  return denote ? hex : `#${hex}`;
 };
 
-const hexToRgb = (input: string): ColorSpace<"rgb"> => {
-  const hex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(input) as [string, string, string, string] | null;
+const hexToRgb = (input: string): ColorFormat<"rgb"> => {
+  const hex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(input) as
+    | [string, string, string, string]
+    | null;
 
   if (!hex) {
-    return [0, 0, 0] as ColorSpace<"rgb">;
+    return ["rgb", 0, 0, 0] as ColorFormat<"rgb">;
   }
 
   const r = Number.parseInt(hex[1], 16);
   const g = Number.parseInt(hex[2], 16);
   const b = Number.parseInt(hex[3], 16);
 
-  return [r, g, b] as ColorSpace<"rgb">;
+  return ["rgb", r, g, b] as ColorFormat<"rgb">;
 };
 
 export { rgbToHex, hexToRgb };
